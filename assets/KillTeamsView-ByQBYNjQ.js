b3c0fe3e43381474b070ec6e5589e8edc6f63954
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2024-02-18 01:24:57 +0100

    Updates

---
+++ assets/KillTeamsView-ByQBYNjQ.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as s}from"./index-D6gL9SYz.js";const o={},t={class:"container"};function n(a,r){return s(),c("div",t,"Kill Teams")}const i=e(o,[["render",n]]);export{i as default};
--- assets/KillTeamsView-O5nHG-e5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as s}from"./index-Pt8C8bkB.js";const o={},t={class:"container"};function n(a,r){return s(),c("div",t,"Kill Teams")}const i=e(o,[["render",n]]);export{i as default};
